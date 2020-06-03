83dde8d2dc25ab919c41fae51ea7bcea2fa95b37
Joohansson <d84@outlook.com>
2020-06-03 21:38:42 +0200

    gh-pages push

--- src/store/app/state.js
+++ src/store/app/state.js
@@ -24,7 +24,7 @@ export function initialState () {
             'Content-Type': 'application/json'
           }
         },
-      }
+      },
     },
     // DO NOT CHANGE ANYTHING BELOW
     privatekey: null,
