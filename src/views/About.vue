a140ff34686f0dcff7ed4fe1798bc980568ce346
Joohansson <d84@outlook.com>
2020-06-15 15:15:39 +0200

    better about

--- src/views/About.vue
+++ src/views/About.vue
@@ -11,21 +11,23 @@
       <div class="about">
         <p>
           TIX is a secure client-side JS Wallet running on the live Nano network.<br>
-          
           <ul>
             <li>Developed by <a href="https://www.linuxserver.io/">LinuxServer.io</a></li>
             <li>Source code at <a href="https://github.com/Joohansson/nano-wallet">Github</a> and this host is running from the <a href="https://github.com/Joohansson/nano-wallet/tree/gh-pages">GH pages branch</a></li>
+            <li>More info at <a href="https://nano.org">Nano.org</a></li>
           </ul> 
-          
         </p>
-        <h2>TERMS</h2>
+
+        <h2>SPECIAL FEATURES</h2>
         <p>
           <ul>
-            <li>By using TIX you agree to the following terms which may update at any time.</li>
-            <li>TIX does not store or send keys anywhere. Keeping a backup of any seed or private key is up to the user. TIX will not be held accountable for any loss of funds.</li>
-            <li>TIX or the owner of any backend API it connects to does not promise a certain uptime and may stop or pause the service at any time.</li>
-          </ul>  
+            <li><router-link :to="$store.getters['app/nodeLink'] + 'generate'">Address generator with paper wallet</router-link></li>
+            <li><router-link :to="$store.getters['app/nodeLink'] + 'block/D318ADB547B69733BAFB7D491BF05B38C542F57A39A8BEDB05657D8C95A9EA6D'">Block Explorer</router-link></li>
+            <li><router-link :to="$store.getters['app/nodeLink'] + 'address/nano_3jwrszth46rk1mu7rmb4rhm54us8yg1gw3ipodftqtikf5yqdyr7471nsg1k'">Watch-only address</router-link></li>
+            <li><router-link :to="$store.getters['app/nodeLink'] + 'pos/nano_1gur37mt5cawjg5844bmpg8upo4hbgnbbuwcerdobqoeny4ewoqshowfakfo'">Point of Sale (PoS) Terminal</router-link></li>
+          </ul>
         </p>
+
         <h2>TECHNICALITY</h2>
         <p>
           <ul>
@@ -36,14 +38,14 @@
             <li>Your account's representative can be changed under Settings.</li>
           </ul>
         </p>
-        <h2>SPECIAL FEATURES</h2>
+
+        <h2>TERMS</h2>
         <p>
           <ul>
-            <li><router-link :to="$store.getters['app/nodeLink'] + 'generate'">Address generator with paper wallet</router-link></li>
-            <li><router-link :to="$store.getters['app/nodeLink'] + 'block/D318ADB547B69733BAFB7D491BF05B38C542F57A39A8BEDB05657D8C95A9EA6D'">Block Explorer</router-link></li>
-            <li><router-link :to="$store.getters['app/nodeLink'] + 'address/nano_3jwrszth46rk1mu7rmb4rhm54us8yg1gw3ipodftqtikf5yqdyr7471nsg1k'">Watch-only address</router-link></li>
-            <li><router-link :to="$store.getters['app/nodeLink'] + 'pos/nano_1gur37mt5cawjg5844bmpg8upo4hbgnbbuwcerdobqoeny4ewoqshowfakfo'">Point of Sale (PoS) Terminal</router-link></li>
-          </ul>
+            <li>By using TIX you agree to the following terms which may update at any time.</li>
+            <li>TIX does not store or send keys anywhere. Keeping a backup of any seed or private key is up to the user. TIX will not be held accountable for any loss of funds.</li>
+            <li>TIX or the owner of any backend API it connects to does not promise a certain uptime and may stop or pause the service at any time.</li>
+          </ul>  
         </p>
       </div>
     </div>
